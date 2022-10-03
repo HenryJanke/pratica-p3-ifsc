24240fad8918cf23c3701b20047af2a6f2ec91d8
Henry <henry.j@aluno.ifsc.edu.br>
2022-10-03 16:43:52 -0300

    adoro pao

---
+++ prog/src/atv/Bicicleta.java
@@ -0,0 +1,5 @@
+package atv;
+
+public class Bicicleta extends Veiculo{
+
+}
---
+++ prog/src/atv/Carro.java
@@ -0,0 +1,8 @@
+package atv;
+
+public class Carro extends Veiculo{
+
+	public String placa;
+	public void ligar() {}
+	public void desligar() {}
+}
